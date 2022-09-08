e665e33aeb2d99096e36ab013fd39f2dd2c97f10
Usuario <Email Usuario>
2022-09-08 13:33:50 -0500

    Ultimos Cambios

--- js/ejercicio_5.js
+++ js/ejercicio_5.js
@@ -21,3 +21,99 @@ window.addEventListener("click", (e) => {
         venmodal.style.display = "none";
     }
 })
+
+/*Acceder al botón para abrir el modal */
+
+let btnmoda = document.getElementById("desplegarModal");
+
+//Acceder ventana modal
+let venmoda = document.getElementById("ventanModal");
+
+//Acceder al botón Cerrar de la ventana modal
+let cermoda = document.querySelector(".cerrarModal");
+
+btnmoda.addEventListener("click", () => {
+    venmoda.style.display = "block";
+})
+
+cermoda.addEventListener("click", () => {
+    venmoda.style.display = "none";
+})
+
+window.addEventListener("click", (e) => {
+    if (e.target == venmodal) {
+        venmoda.style.display = "none";
+    }
+})
+
+// /*Acceder al botón para abrir el modal */
+
+// let btnmodal = document.getElementById("abrirModal");
+
+// //Acceder ventana modal
+// let venmodal = document.getElementById("ventanaModal");
+
+// //Acceder al botón Cerrar de la ventana modal
+// let cermodal = document.querySelector(".cerrarModal");
+
+// btnmodal.addEventListener("click", () => {
+//     venmodal.style.display = "block";
+// })
+
+// cermodal.addEventListener("click", () => {
+//     venmodal.style.display = "none";
+// })
+
+// window.addEventListener("click", (e) => {
+//     if (e.target == venmodal) {
+//         venmodal.style.display = "none";
+//     }
+// })
+
+// /*Acceder al botón para abrir el modal */
+
+// let btnmodal = document.getElementById("abrirModal");
+
+// //Acceder ventana modal
+// let venmodal = document.getElementById("ventanaModal");
+
+// //Acceder al botón Cerrar de la ventana modal
+// let cermodal = document.querySelector(".cerrarModal");
+
+// btnmodal.addEventListener("click", () => {
+//     venmodal.style.display = "block";
+// })
+
+// cermodal.addEventListener("click", () => {
+//     venmodal.style.display = "none";
+// })
+
+// window.addEventListener("click", (e) => {
+//     if (e.target == venmodal) {
+//         venmodal.style.display = "none";
+//     }
+// })
+
+// /*Acceder al botón para abrir el modal */
+
+// let btnmodal = document.getElementById("abrirModal");
+
+// //Acceder ventana modal
+// let venmodal = document.getElementById("ventanaModal");
+
+// //Acceder al botón Cerrar de la ventana modal
+// let cermodal = document.querySelector(".cerrarModal");
+
+// btnmodal.addEventListener("click", () => {
+//     venmodal.style.display = "block";
+// })
+
+// cermodal.addEventListener("click", () => {
+//     venmodal.style.display = "none";
+// })
+
+// window.addEventListener("click", (e) => {
+//     if (e.target == venmodal) {
+//         venmodal.style.display = "none";
+//     }
+// })
