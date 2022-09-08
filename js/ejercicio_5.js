2f3c90363ad2f213824899d6bd057af774f15552
DanielSierra17 <dfsierra898@misena.edu.co>
2022-09-08 10:19:05 -0500

    Ultimos Cambios

--- js/ejercicio_5.js
+++ js/ejercicio_5.js
@@ -1,23 +1,23 @@
 /*Acceder al botón para abrir el modal */
 
-let btnmodal=document.getElementById("abrirModal");
+let btnmodal = document.getElementById("abrirModal");
 
 //Acceder ventana modal
-let venmodal=document.getElementById("ventanaModal");
+let venmodal = document.getElementById("ventanaModal");
 
 //Acceder al botón Cerrar de la ventana modal
-let cermodal=document.querySelector(".cerrarModal");
+let cermodal = document.querySelector(".cerrarModal");
 
-btnmodal.addEventListener("click",()=>{
-    venmodal.style.display="block";
+btnmodal.addEventListener("click", () => {
+    venmodal.style.display = "block";
 })
 
-cermodal.addEventListener("click",()=>{
-    venmodal.style.display="none"; 
+cermodal.addEventListener("click", () => {
+    venmodal.style.display = "none";
 })
 
-window.addEventListener("click",(e)=>{
-if(e.target==venmodal){
-    venmodal.style.display="none"; 
-}
+window.addEventListener("click", (e) => {
+    if (e.target == venmodal) {
+        venmodal.style.display = "none";
+    }
 })
