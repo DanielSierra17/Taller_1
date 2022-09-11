3fd8c8ce2decdb7f16670ec00060bffb96498320
DanielSierra17 <dfsierra898@misena.edu.co>
2022-09-11 15:48:38 -0500

    Adelanto Validaciones

---
+++ js/ejercicio_6.js
@@ -0,0 +1,28 @@
+let = document.getElementById("nd").value
+let nom = document.getElementById("no").value
+let ape = document.getElementById("ap").value
+let fech = document.getElementById("fn").value
+let email = document.getElementById("ce").value
+let clv = document.getElementById("ct").value
+let vclv = document.getElementById("vc").value
+let btv = document.getElementById("bv")
+
+function validarFormulario() {
+
+    if (condition) {
+
+    }
+
+    else if (condition) {
+
+    }
+
+    else {
+
+    }
+
+    alert(`$`)
+    console.log(`$`)
+}
+
+btv.addEventListener('click', validarFormulario)
